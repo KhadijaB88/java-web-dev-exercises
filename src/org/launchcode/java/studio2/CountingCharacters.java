a40f3bab9e7f6a03c5b3e930ed61c7d8988b455d
Khadija Bano <kbano88@gmail.com>
2021-07-21 20:21:57 -0500

    "Studio done with some bonus"

--- src/org/launchcode/java/studio2/CountingCharacters.java
+++ src/org/launchcode/java/studio2/CountingCharacters.java
@@ -1,11 +1,18 @@
 package org.launchcode.java.studio2;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
 
     public static void main(String[] args){
-        String inputStr = "";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+//        String inputStr = "I love Java!";
+        String inputStr = input.nextLine();
+        inputStr = inputStr.toUpperCase();
         char[] charInString = inputStr.toCharArray();
         HashMap<Character, Integer> charCount = new HashMap<>();
 
@@ -13,6 +20,12 @@ public class CountingCharacters {
             if(charCount.containsKey(letter)){
                 charCount.put(letter, charCount.get(letter)+1);
             }
+            else{
+                charCount.put(letter, 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> printChar : charCount.entrySet()){
+            System.out.println(printChar.getKey()+ " : " + printChar.getValue());
         }
 
     }
