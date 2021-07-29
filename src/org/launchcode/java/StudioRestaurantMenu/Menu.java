c984001b81a35b16028f4b3a747588ace6337ea9
Khadija Bano <kbano88@gmail.com>
2021-07-28 22:03:20 -0500

    "Studio 4.8.3"

--- src/org/launchcode/java/StudioRestaurantMenu/Menu.java
+++ src/org/launchcode/java/StudioRestaurantMenu/Menu.java
@@ -1,4 +1,20 @@
 package org.launchcode.java.StudioRestaurantMenu;
 
+import java.util.ArrayList;
+
 public class Menu {
+//utilize the date class
+    private ArrayList<MenuItem> listOfItems;
+
+    public Menu(ArrayList<MenuItem> listOfItems){
+        this.listOfItems = listOfItems;
+    }
+
+    public ArrayList<MenuItem> getListOfItems() {
+        return listOfItems;
+    }
+
+    public void setListOfItems(ArrayList<MenuItem> listOfItems) {
+        this.listOfItems = listOfItems;
+    }
 }
