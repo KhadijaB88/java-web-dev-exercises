55f332af121286a8b656397ddd1740abeef8d4ae
Khadija Bano <kbano88@gmail.com>
2021-08-25 14:54:01 -0500

    "Exercise 8 done without bonus"

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,8 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -8,12 +10,24 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
+        Comparator comparator = new FlavorComparator();
+        Comparator coneCost = new ConeComparator();
+
+
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
 
+//        flavors.sort(comparator);
+        flavors.sort(new FlavorComparator());
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+//        cones.sort(coneCost);
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        System.out.println(flavors);
+        System.out.println(cones);
     }
 }
